2d072d0f5db2ea4161e9cd8ba6c3f04e95d3f7f3
Ryan Yue Chun <ryan.yue-chun@outlook.com>
2020-01-03 00:36:24 +0100

    ScriptStart modified

--- ScriptStart.sh
+++ ScriptStart.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# This command takes a path to the json file as a parameter
 getJsonContent() {
-	cat $1
+	echo "`cat $1`"
 }
 
+# This command takes the url of the put API as the first parameter and the Json body as the second parameter
+putJsonData() {
+	curl -X PUT "$1" -H 'Content-Type: application/json' -d\'$2\'
+}
 
+putJsonData $1 $(getJsonContent $2)
